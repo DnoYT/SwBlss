f91c1f5ccd37fe2ca89c60057a070152ca26df5d
DnoYT <56258350+DnoYT@users.noreply.github.com>
2020-12-23 11:50:43 +0800

    更改语言

--- SwBlss.cpp
+++ SwBlss.cpp
@@ -37,6 +37,8 @@ typedef struct {
     char menu0[50];
    	char menu1[50];
    	char menu[20][50];
+   	char sr[2][50];
+   	char choice[60];
 };
 
 
@@ -46,7 +48,7 @@ typedef struct {
 LS LS_CN;
 LS LS_EU;
 LS LS_DF;
-char kg[] = "\t\t\t";
+char kg[] = "\t\t";
 
 
 //********************注册函数 
@@ -68,6 +70,19 @@ void gui_menu();
 void gui_color();
 
 
+//功能模块
+void func();
+//功能 学生信息添加 
+void func_add();
+//功能 学生信息删除 
+void func_Delete();
+//功能 关于我们 
+void func_Abou();
+//功能 退出 
+void func_Exit();
+
+
+
 using namespace std;
 
 
@@ -85,10 +100,14 @@ int main(int argc, char** argv) {
 
 	//绘画菜单 
 	gui_menu();
-		
+	
+	//进行功能判断
+	func();
 	//CHelloWorld();用c语言输出HelloWorld 
 	return 0;
 }
+
+
 
 
 
@@ -110,10 +129,12 @@ void loaLang_CN(){
 	strcpy(La.menu[i] ,"删除");
 	i++;
 	strcpy(La.menu[i] ,"关于");
-	strcpy(La.YXlang ,"你选择了中文");
+	//strcpy(La.YXlang ,"你选择了中文");
 	strcpy(La.YXlang ,"欢迎使用广西外国语学院学生管理系统——四维变量团队开发");
-	printf() 
-	strcpy(La.YXlang ,"请输入");
+	strcpy(La.sr[0] ,"请输入");
+	strcpy(La.sr[1] ,"选择功能");
+	strcpy(La.choice ,"你选择了功能：");
+	
 	LS_CN = La;
 }
 
@@ -130,9 +151,11 @@ void loaLang_EU(){
 	strcpy(La.menu[i] ,"DELETE");
 	i++;
 	strcpy(La.menu[i] ,"Abou");
-	strcpy(La.YXlang ,"You chose English");
-	strcpy(La.YXlang ,"欢迎使用广西外国语学院学生管理系统——四维变量团队开发");
-	
+	//strcpy(La.YXlang ,"You chose English");
+	strcpy(La.YXlang ,"Welcome to 广西外国语学院 Student management system");
+	strcpy(La.sr[0] ,"Please enter");
+	strcpy(La.sr[1] ,"Select function");
+	strcpy(La.choice ,"You chose the feature：");
 	LS_EU = La;
 }
 
@@ -155,7 +178,7 @@ int lang_if(){
 	
 	
 	//中文提醒 
-	char remind[] =  "请选择语言(Please select language)\n";
+	char remind[] =  "请选择语言(Please select language)\n随便输入默认 中文";
 	
 	char remind_sancf[] = "请输入EU/CN:";
 	
@@ -169,31 +192,20 @@ int lang_if(){
 	//加载语言; 
 	//loadLang(remind_input);
 	
-	
-	
 	//小写转大写
 	 strcpy(remind_input , strupr(remind_input));
 	
 	//的输入值进行判断 
-	if (strcmp(remind_input,"CN") ==0 ){
-		gui_cls();
+	gui_cls();
+	if (strcmp(remind_input,"CN") == 0){
 		LS_DF = LS_CN; 
-		cout<<LS_DF.YXlang<<endl;
-		return 0;
-	}
-	
-	if (strcmp(remind_input,"EU") ==0 ){
-		gui_cls();
+	}else if (strcmp(remind_input,"EU") == 0){
 		LS_DF = LS_EU; 
-		cout<<LS_DF.YXlang<<endl;
-		return 0;
-	}
-	
-	
-		gui_cls();
+	}else{
 		LS_DF = LS_CN; 
 		cout<<"默认中文"<<endl;
-	gui_cls();
+	}
+		cout<<LS_DF.YXlang<<endl;
 }
  
  
@@ -222,7 +234,10 @@ void gui_menu(){
 		cout<<kg<<i<<"."<<LS_DF.menu[i];
 		i++;
 	}
+	//退出在最后一个 
 	cout<<endl<<kg<<"0."<<LS_DF.menu[0];
+	//提醒输出大小
+	printf("\n%s【0-%d】%s：",LS_DF.sr[0],i-1,LS_DF.sr[1]); 
 }
 
 //清空界面 
@@ -234,5 +249,59 @@ void gui_cls(){
 void gui_color(){
 	system("color 30"); 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
- 
+//主功能 
+void func(){
+	int func_id;
+	cin >> func_id;
+	gui_cls();
+	cout<<LS_DF.choice<<func_id;
+}
+
+
+//功能 学生信息添加 
+void func_add(){
+	
+} 
+
+//功能 学生信息删除 
+void func_Delete(){
+	
+}
+
+
+//功能 关于我们 
+void func_Abou(){
+	
+}
+
+
+//功能 退出 
+void func_Exit(){
+	
+}
