d15049a45a262fd6b1f186dff60c7cc925c16c2f
Ytian <1545496702@qq.com>
2021-01-07 06:51:38 +0800

    添加特别说明

--- SwBlss.cpp
+++ SwBlss.cpp
@@ -1,3 +1,9 @@
+//特别声明 该项目百分之80代码是四维变量 独立开发
+//该项目是本团队大学期末考试作品 你使用可以使用但是请不要影响我们
+//该项目链接：https://github.com/DnoYT/SwBlss
+//特别感谢 王耀强老师 李志军老师 张杨老师 的大力支持
+//该项目已经可以使用 预计完成度 80%
+
 #include <iostream>
 #include <windows.h>
 #include <cstring>
